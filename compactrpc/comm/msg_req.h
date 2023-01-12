1c35c277a9e631353b8abbaa974f92b9148c018b
YogaGuo <3376383434@qq.com>
2023-01-12 22:48:07 +0800

    finish Utill Generator msg  if msg is empty

---
+++ compactrpc/comm/msg_req.h
@@ -0,0 +1,16 @@
+#ifndef COMPACTRPC_MSG_REQ_H
+#define COMPACTRPC_MSG_REQ_H
+#include <string>
+
+namespace compactrpc
+{
+
+    class MsgReqUtil
+    {
+    public:
+        static std::string genMsgNumber();
+    };
+
+}
+
+#endif
